67fe17c426c4b588d360d63fba40deca2bd2d70d
Alain Ravet <alainravet@gmail.com>
2012-07-20 22:40:50 +0200

    the todo_next(<<TEXT) .. command generate executable RSpec pending tests
    this text :
    todo_next(<<TEXT)
    The Foo
      - has a bar
      - has a buz
    TEXT
    
    generates this code :
    
    describe "The Foo" do
      it("- has a bar", :pending => "- has a bar"){}
      it("- has a buz", :pending => "- has a buz"){}
    end

--- lib/todo_next.rb
+++ lib/todo_next.rb
@@ -5,3 +5,22 @@ module TodoNext
 end
 require File.dirname(__FILE__) + '/todo_next/parser'
 require File.dirname(__FILE__) + '/todo_next/tree/depth_first_visitor_base'
+
+require File.dirname(__FILE__) + '/todo_next/rspec_generator_visitor'
+
+main = TOPLEVEL_BINDING.eval('self')
+def main.todo_next(text, puts_code=true)
+  code = TodoNext(text)
+  if puts_code
+    puts '---' ; puts code  ; puts '---'
+  end
+  eval code
+end
+
+def TodoNext(source)
+  TodoNext::Parser.
+      parse(source).
+      visit(TodoNext::RspecGeneratorVisitor.new).
+      flatten.
+      join("\n")
+end
